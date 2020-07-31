8d711344b6729c6c7e3d255b8475269429c08352
tetrix1993 <tetrix1993@hotmail.com>
2020-07-31 16:18:58 +0800

    Query only items that are in input.txt

--- query.py
+++ query.py
@@ -1,5 +1,6 @@
 import sqlite3
 import queries
+import os
 from constants import *
 
 conn = sqlite3.connect(DB_LOCATION)
@@ -7,6 +8,7 @@ c = conn.cursor()
 
 
 def run():
+    item_ids = get_item_ids()
     print_intro_message()
     choice = input('Enter choice: ')
     while True:
@@ -20,7 +22,7 @@ def run():
         elif choice == '3':
             query_by_item_id()
         elif choice == '4':
-            query_by_series_id()
+            query_by_series_id(item_ids)
         else:
             print('Invalid choice. Exiting...')
             return
@@ -40,26 +42,31 @@ def print_intro_message():
 def query_by_item_id():
     query = input('Enter Item ID: ').strip().replace(' ', '')
     if len(query) > 0:
-        c.execute(queries.SELECT_RELEASE_DATE_BY_ITEM_ID, (query, ))
+        c.execute(queries.SELECT_RELEASE_DATE_BY_ITEM_ID, (query.upper(), ))
         item = c.fetchone()
         if item is not None:
             print(item)
         else:
-            print('Item ID %s not found' % query)
+            print('Item ID %s not found' % query.upper())
     else:
         print('Invalid input')
 
 
-def query_by_series_id():
+def query_by_series_id(item_ids):
     query = input('Enter Series ID: ').strip().replace(' ', '')
     if len(query) >= 5:
-        c.execute(queries.SELECT_RELEASE_DATE_BY_SERIES, (query + '%', ))
+        c.execute(queries.SELECT_RELEASE_DATE_BY_SERIES, (query.upper() + '%', ))
         item_list = c.fetchall()
         if len(item_list) > 0:
+            has_print_item = False
             for item in item_list:
-                print(item)
+                if item[0] in item_ids:
+                    has_print_item = True
+                    print(item)
+            if not has_print_item:
+                print('Items with Series ID %s matched but not found in ' % query.upper() + INPUT_FILE_PATH)
         else:
-            print('Items with Series ID %s not matched' % query)
+            print('Items with Series ID %s not matched' % query.upper())
     else:
         print('Invalid input')
 
@@ -72,6 +79,20 @@ def list_items(by_release_dt=True):
     item_list = c.fetchall()
     for item in item_list:
         print(item)
+
+
+def get_item_ids():
+    item_ids = []
+    if not os.path.exists(INPUT_FILE_PATH):
+        pass
+    else:
+        with open(INPUT_FILE_PATH, 'r') as f:
+            line = f.readline()
+            while line:
+                if len(line.strip()) > 0:
+                    item_ids.append(line.strip())
+                line = f.readline()
+    return item_ids
 
 
 if __name__ == '__main__':
